71eec346c5f2c8cd312a55d0ed2e4047bfc07ed1
“ChatBot <ChatBot.Kenya@clclearningafrica.com>
2023-02-19 20:17:10 +0300

    all the files required for deployment into aws

---
+++ Modules/Networking/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "private_cidrs" {}
+variable "public_cidrs" {}
+variable "vpc_cidr" {}
+
+variable "access_ip" {
+  type = string
+}
